6b08c3f2ce9928e305759bef7008c97209bce92c
RosieOh <20172207@gm.hannam.ac.kr>
2023-09-05 15:26:32 +0900

    node.js 데이터 바인딩

--- js01.js
+++ js01.js
@@ -1,1 +1,7 @@
-console.log("Hello~! NodeJS~!")
+var a = 20; // 재선언 O, 변수값 변경 O
+let b = 200; // 재선언 x, 변수값 변경 O
+const c = 2000; // 재선언 x, 변수값 변경 x
+// 백틱(backtick = ``)으로 감싼 문장 => 템플릿(template)
+console.log("Hello~! NodeJS~!");
+console.log("var a : "+a+", let b:"+b+", const : "+c);
+console.log(`var a : ${a}, let b : ${b}, const c: ${c}`);
